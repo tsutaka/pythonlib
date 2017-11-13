9814a4d75b1d065dcee343012256e0f9d6ac7491
tsutaka <mitsutaka5897@yahoo.co.jp>
2017-11-14 00:46:26 +0900

    update for some files

--- resize.py
+++ resize.py
@@ -1,24 +1,37 @@
+import os
+
 import cv2
-import numpy as np
+# import numpy as np
 
-def half_size(im):
-        height = im.shape[0]
-        width = im.shape[1]
-        
-        cv2.imshow("full_size",im)
+def half_size(images):
+    
+    for image in images:
+        height = image.shape[0]
+        width = image.shape[1]
+                
+        cv2.imshow("full_size",image)
         cv2.waitKey(0)
-        cv2.destroyAllWindows()  
+        cv2.destroyAllWindows() 
         
-        half_size = cv2.resize(im,(int(width/2),int(height/2)))
+        half_image = cv2.resize(image,(int(width/2),int(height/2)))
 
-        cv2.imshow("half_size",half_size)
+        cv2.imshow("half_size",half_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()  
 
 if __name__ == '__main__':
-    im = cv2.imread("image.jpg")
-    if not (im.all() == None):
-        half_size(im)
+    
+    paths = os.listdir('.\image')
+    print(paths)
+    print(len(paths))
+
+    images = []
+    for path in paths:
+        images.append(cv2.imread(path))
+
+    if not (images[-1].all() == None):
+        half_size(images)
     else:
         print('Not exist')
+        exit()
         
