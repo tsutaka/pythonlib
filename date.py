95a31b62e2d62d2558c4f3deda8d6f3512a44e2b
tsutaka <tomasen3@gmail.com>
2018-05-02 23:30:55 +0900

    [add]日付の取得サンプルの追加

---
+++ date.py
@@ -0,0 +1,21 @@
+import datetime
+
+def get_today():
+    return datetime.date.today()
+
+def add_days(input_date, add_days):
+    return input_date + datetime.timedelta(days = add_days)
+
+def date_format(date_):
+    return "%d/%d" % (date_.month, date_.day)
+
+
+if __name__ == '__main__':
+
+    today = get_today()
+    today_str = date_format(today)
+    print(today_str)
+
+    tomorrow = add_days(today, 1)
+    tomorrow_str = date_format(tomorrow)
+    print(tomorrow_str)
