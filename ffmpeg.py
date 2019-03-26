28b77d30ad7dc477f2546143bb873900d5225ee0
tsutaka <tomasen3@gmail.com>
2019-03-27 00:29:58 +0900

    [add]cut movie

---
+++ ffmpeg.py
@@ -0,0 +1,34 @@
+# ffmpeg.py
+# ffmpegを使用して映像から画像を切り出す
+import sys          # コマンドライン引数
+import subprocess   # コマンド実行
+
+# ffmpegをインストールしパスを通す必要がある
+
+def movie_to_images(input_path, start_seconds, end_seconds, interval):
+
+    #ex. ffmpeg -i sample_movie.mp4 -ss 0 -t 19 -r 1 -q:v 1 %06d.jpg
+    cmd = "ffmpeg -i " + input_path + " -ss " + start_seconds + \
+        " -t " + end_seconds + " -r " + interval + " -q:v 1 %06d.jpg"
+    subprocess.call(cmd)
+
+    return
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    # ex. python ffmpeg.py test.mp4 0 10 1
+    args = sys.argv
+    input_path = args[1]
+    start_seconds = args[2]
+    end_seconds = args[3]
+    interval = args[4]
+    if(len(args) != 5) :
+        print("args not have 4 arg")
+
+    print("input_path:", input_path)
+    print("start_seconds:", start_seconds)
+    print("end_seconds:", end_seconds)
+    print("interval:", interval)
+    
+    movie_to_images(input_path, start_seconds, end_seconds, interval)
