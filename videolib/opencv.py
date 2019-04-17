6ce63219cc61eb30393bf6c76dfca17938df30c7
tsutaka <tomasen3@gmail.com>
2019-04-17 21:54:12 +0900

    [feat] add check diff images

--- videolib/opencv.py
+++ videolib/opencv.py
@@ -5,50 +5,44 @@ import cv2          # for using Opencv
 
 # before use, requires install opencv with pip
 # pip install opencv-python
-# pip install opencv-contrib-python
+# pip install opencv-contrib-python --user
 
-def check_diff(image_path):
-    # input directory's style is following:
-    # 000001.jpg, 000002.jpg, 000003.jpg
-
-    # result_list = {}
-    
-    # get jpg file name
-
-
+def check_diff(input_path1, input_path2, output_path):
     # load images
-    img_src1 = cv2.imread("sample/000001.jpg", 1)
-    img_src2 = cv2.imread("sample/000002.jpg", 1)
+    img_src1 = cv2.imread(input_path1, 1)
+    img_src2 = cv2.imread(input_path2, 1)
 
+    # check diff
     fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
-    #fgbg = cv2.createBackgroundSubtractorMOG()
 
     fgmask = fgbg.apply(img_src1)
     fgmask = fgbg.apply(img_src2)
 
-    # 表示
-    cv2.imshow('frame',fgmask)
-
-    # 検出画像
-    bg_diff_path  = './sample/diff.jpg'
+    # save image
+    bg_diff_path  = output_path
     cv2.imwrite(bg_diff_path,fgmask)
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    # check diff
+    # show
+    # cv2.imshow('frame',fgmask)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     return
 
 
 if __name__ == "__main__":
     # execute only if run as a script
-    # ex. python opencv.py sample/
+    # ex. python opencv.py sample/000001.jpg sample/000002.jpg sample/diff000001.jpg
     args = sys.argv
-    if(len(args) != 2) :
+    if(len(args) != 1 + 3) :
         print("args not have 1 arg")
-    input_path = args[1]
+        exit()
+    input_path1 = args[1]
+    input_path2 = args[2]
+    output_path = args[3]
 
-    print("input_path:", input_path)
+    print("input_path1:", input_path1)
+    print("input_path2:", input_path2)
+    print("output_path:", output_path)
     
-    check_diff(input_path)
+    check_diff(input_path1, input_path2, output_path)
