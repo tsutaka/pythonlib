047ce9178ca58094934f848b4687cc598c2587ae
tsutaka <tomasen3@gmail.com>
2019-05-06 01:06:25 +0900

    [feat] add new source for subprocessing

---
+++ diff_proc.py
@@ -0,0 +1,57 @@
+"""
+diff_proc.py
+"""
+import os # for handling directory
+import re # for using regular expression
+import sys # for command line arguments
+import json # for using json
+
+# internal import
+from videolib.opencv import check_diff
+
+def diff_proc(target_path):
+    """
+    make diff.json 
+    """
+    output_json = {}
+    prev_file_name = ""
+
+    jpeg_list = []
+    files = os.listdir(target_path)
+    for file_str in files:
+        if(re.findall(r'^(\d){6}\.jpg$', file_str)):
+            jpeg_list += [file_str]
+
+    for file_name in jpeg_list:
+        if prev_file_name == "":
+            prev_file_name = file_name
+            continue
+
+        diff_frame, full_frame = check_diff(
+            target_path + os.sep + prev_file_name,
+            target_path + os.sep + file_name)
+
+        output_json[str(file_name)] = {
+            'target':prev_file_name,
+            'diff':diff_frame,
+            'full':full_frame}
+
+        prev_file_name = file_name
+
+    # print("{}".format(json.dumps(output_json, indent=4)))
+    output_fp = open(
+        target_path + os.sep +
+        'diff.json', 'w')
+    json.dump(output_json, output_fp, indent=4)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    # ex. python diff_proc.py ./assets/sample1
+    args = sys.argv
+    if(len(args) != 1 + 1):
+        exit()
+    path = args[1]
+
+    print("path:", path)
+
+    diff_proc(path)
