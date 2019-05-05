bb40d29c10806dd743474350237563a3b275afe0
tsutaka <tomasen3@gmail.com>
2019-05-05 20:18:11 +0900

    [feat] add get diff function

--- videolib/opencv.py
+++ videolib/opencv.py
@@ -1,13 +1,21 @@
-# opencv.py
-import sys          # for command line arguments
-import subprocess   # for command executing
-import cv2          # for using Opencv
+"""
+opencv.py
 
-# before use, requires install opencv with pip
-# pip install opencv-python
-# pip install opencv-contrib-python --user
+Notes
+-----
+before use, requires install opencv with pip
+pip install opencv-python
+pip install opencv-contrib-python --user
+"""
+import sys          # for command line arguments
+import cv2          # for using Opencv
+from PIL import Image # for using PIL format
+
 
 def check_diff(input_path1, input_path2, output_path):
+    """
+    return num of diff pixel
+    """
     # load images
     img_src1 = cv2.imread(input_path1, 1)
     img_src2 = cv2.imread(input_path2, 1)
@@ -15,51 +23,65 @@ def check_diff(input_path1, input_path2, output_path):
     # check diff
     fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
 
-    fgmask = fgbg.apply(img_src1)
-    fgmask = fgbg.apply(img_src2)
+    diff_image = fgbg.apply(img_src1)
+    diff_image = fgbg.apply(img_src2)
 
     # save image
-    bg_diff_path  = output_path
-    cv2.imwrite(bg_diff_path,fgmask)
+    # bg_diff_path = output_path
+    # cv2.imwrite(bg_diff_path, diff_image)
 
-    # show
-    # cv2.imshow('frame',fgmask)
+    # show image
+    # cv2.imshow('frame',diff_image)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 
-    return
+    # calcurate diff
+    diff_pixels = 0
+    image_pil = Image.fromarray(diff_image)
+    image_pil = image_pil.convert('RGB')
+    image_size = image_pil.size
+    print(image_size)
+    for index_y in range(int(image_size[1])):
+        for index_x in range(int(image_size[0])):
+            red, green, blue = image_pil.getpixel((index_x, index_y))
+            if(blue == 255 and green == 255 and red == 255):
+                diff_pixels += 1
 
-def threshold(input_path, output_path, threshold):
+    return diff_pixels, int(image_size[0]) * int(image_size[1])
 
-    img = cv2.imread(input_path,0)
-    img = cv2.medianBlur(img,5)
+def binarization(input_path, output_path, threshold):
+    """
+    return image of binarization
+    """
+    img = cv2.imread(input_path, 0)
+    img = cv2.medianBlur(img, 5)
 
-    ret,th1 = cv2.threshold(img,threshold,255,cv2.THRESH_BINARY)
-    # th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-    #             cv2.THRESH_BINARY,11,2)
-    # th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-    #             cv2.THRESH_BINARY,11,2)
-    
+    ret, th1 = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
+    print(ret)
+
     # save image
     # cv2.imwrite(output_path,th1)
-    cv2.imshow("test", th1)
+
+    # show image
+    cv2.imshow("mono", th1)
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     # execute only if run as a script
     # ex. python opencv.py sample/000001.jpg sample/000002.jpg sample/diff000001.jpg
     args = sys.argv
-    if(len(args) != 1 + 3) :
-        print("args not have 1 arg")
+    if(len(args) != 1 + 3):
+        print("args not have 3 arg")
         exit()
-    input_path1 = args[1]
-    input_path2 = args[2]
-    output_path = args[3]
+    in1 = args[1]
+    in2 = args[2]
+    out = args[3]
 
-    print("input_path1:", input_path1)
-    print("input_path2:", input_path2)
-    print("output_path:", output_path)
-    
-    # check_diff(input_path1, input_path2, output_path)
+    print("in1:", in1)
+    print("in2:", in2)
+    print("out:", out)
 
-    threshold(input_path1, output_path, 127)
+    check_diff(in1, in2, out)
+
+    # binarization(in1, out, 127)
