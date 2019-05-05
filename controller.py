0f42f90f29311432e62957c2f0de689c09c22d3b
tsutaka <tomasen3@gmail.com>
2019-05-05 15:23:06 +0900

    [feat] add cut function

--- controller.py
+++ controller.py
@@ -4,6 +4,7 @@ import re # for using regular expression
 
 # internal import
 from videolib.ffmpeg import movie_full_to_images
+from videolib.ffmpeg import get_movie_duration
 
 COLOR_GREEN = 3
 COLOR_LIGHTGREEN = 11
@@ -21,7 +22,7 @@ class VideoCtrl:
 
     MENU_MAX = 8
     INPUT_WAIT = 5
-    INPUT_LONG_WAIT = 50
+    INPUT_LONG_WAIT = 10
 
     VIDEO_PATH = "./assets"
 
@@ -33,12 +34,26 @@ class VideoCtrl:
             
         self.cursor_pos = 0
         self.cursor_offset = 0
-        self.input_wait_counter = 0
+        self.input_wait_counter = 0 
+        self.processing_file = ""
+        self.duration = 0
 
         pyxel.run(self.update, self.draw)
 
 
     def update(self):
+
+        # common process
+        if(self.input_wait_counter > 0):
+            self.input_wait_counter -= 1
+            
+        path = VideoCtrl.VIDEO_PATH
+        files = os.listdir(path)
+        files_dir = [f for f in files if os.path.isdir(os.path.join(path, f))]
+    
+        if(self.cursor_pos + self.cursor_offset < len(files_dir)):  
+            target_path = files_dir[self.cursor_pos + self.cursor_offset]
+
         # quit app
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
@@ -63,22 +78,29 @@ class VideoCtrl:
             elif(self.cursor_offset > 0):
                 self.cursor_offset -= 1
             
-        if(self.input_wait_counter > 0):
-            self.input_wait_counter -= 1
-        
         # cut video
         if pyxel.btn(pyxel.KEY_C) and self.input_wait_counter == 0:
             self.input_wait_counter = VideoCtrl.INPUT_LONG_WAIT
 
+            video_path = VideoCtrl.VIDEO_PATH + os.sep + target_path
+            input_file = target_path + ".mp4"
+            if(os.path.exists(video_path)):
+                self.processing_file = target_path
+                self.duration = get_movie_duration(video_path, input_file)
+                movie_full_to_images(video_path, input_file, 1)
 
     def draw(self):
+        # common process
         pyxel.cls(0)
 
-        # draw video list
         path = VideoCtrl.VIDEO_PATH
         files = os.listdir(path)
         files_dir = [f for f in files if os.path.isdir(os.path.join(path, f))]
+
+        if(self.cursor_pos + self.cursor_offset < len(files_dir)):  
+            target_path = files_dir[self.cursor_pos + self.cursor_offset]
         
+        # draw video list
         for index_y in range(VideoCtrl.MENU_MAX):
             if(index_y + self.cursor_offset < len(files_dir)):  
                 pyxel.text(
@@ -97,18 +119,36 @@ class VideoCtrl:
         if(self.cursor_offset + VideoCtrl.MENU_MAX < len(files_dir)):
             pyxel.rect(0, 56, 1, 57, COLOR_GREEN)
 
-        # draw video detail
+        # draw time line bar
         pyxel.rectb(
-            1, 
+            0, 
             VideoCtrl.VIDEO_DETAIL_OFFSET_Y, 
             VideoCtrl.SCREEN_WIDTH - 1, 
             VideoCtrl.VIDEO_DETAIL_OFFSET_Y + 5 * 2 - 1, 
             COLOR_GREEN)
-        
-        disp = []
 
-        if(self.cursor_pos + self.cursor_offset < len(files_dir)):  
-            target_path = files_dir[self.cursor_pos + self.cursor_offset]
+        jpeg_list = self.get_file(target_path, r'^(\d){6}\.jpg$') # nnnnnn.jpg
+        if(self.duration > len(jpeg_list) and target_path == self.processing_file):
+            line_max = self.duration
+        elif(self.duration <= len(jpeg_list) and target_path == self.processing_file):
+            line_max = len(jpeg_list)
+            self.duration = 0
+        else:
+            line_max = len(jpeg_list)
+
+        for jpeg_file in jpeg_list:
+            file_index = int(jpeg_file[:6]) - 1
+            pyxel.rect(
+                (VideoCtrl.SCREEN_WIDTH - 1) * file_index / line_max, 
+                VideoCtrl.VIDEO_DETAIL_OFFSET_Y, 
+                (VideoCtrl.SCREEN_WIDTH - 1) * (file_index + 1) / line_max - 1, 
+                VideoCtrl.VIDEO_DETAIL_OFFSET_Y + 5 * 2 - 1, 
+                COLOR_GREEN)
+
+        
+        # draw video detail
+
+        disp = []
 
         disp += [
             "mp4:" + 
@@ -116,14 +156,23 @@ class VideoCtrl:
             ]
 
         disp += [
-            "jpg:" + 
-            ("ok" if(self.check_file(target_path, 'jpg$')) else "no")
+            "jpg(" + 
+            str(len(self.get_file(target_path, r'^(\d){6}\.jpg$'))) + # nnnnnn.jpg
+            ")<c>:" + 
+            ("ok" if(self.check_file(target_path, r'^(\d){6}\.jpg$')) else "no") # nnnnnn.jpg
             ]
 
         disp += [
-            "diff:" + 
-            ("ok" if(self.check_file(target_path, '^diff')) else "no")
+            "diff(" + 
+            str(len(self.get_file(target_path, r'^diff(\d){6}\.jpg$'))) + # diffnnnnnn.jpg            
+            "):" + 
+            ("ok" if(self.check_file(target_path, r'^diff(\d){6}\.jpg$')) else "no")
             ]
+
+        disp += ["incnt:" + str(self.input_wait_counter)]
+
+        disp += ["duration:" + str(self.duration)]
+            
 
         self.disp_list(
             disp, 
